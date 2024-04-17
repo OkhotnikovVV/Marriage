114f6441c8498075b7b35e30171b70df213bc1d1
Professional <8_00@bk.ru>
2024-04-17 17:50:14 +0300

    add tests

---
+++ tests/test_relevant.py
@@ -0,0 +1,6 @@
+class TestViews:
+    # def test_index(self):
+    #     assert 1 == 0
+
+    def test_index2(self):
+        assert 1 == 1
---
+++ tests/test_services.py
@@ -0,0 +1,2 @@
+def test_calculation_logic():
+    assert False
