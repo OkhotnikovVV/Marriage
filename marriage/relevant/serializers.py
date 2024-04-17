3ef5b29356c05f6f3e50f5e8d9dfd7cbbb84576a
Professional <8_00@bk.ru>
2024-04-17 17:25:59 +0300

    update database

--- marriage/relevant/serializers.py
+++ marriage/relevant/serializers.py
@@ -4,8 +4,8 @@ from rest_framework import serializers
 class InputDataSerializer(serializers.Serializer):
     age_from = serializers.IntegerField(min_value=18, max_value=101)
     age_to = serializers.IntegerField(min_value=18, max_value=101)
-    salary_from = serializers.IntegerField(min_value=0, max_value=3000000)
-    salary_to = serializers.IntegerField(min_value=0, max_value=3000000)
+    salary_from = serializers.IntegerField(min_value=0, max_value=3000001)
+    salary_to = serializers.IntegerField(min_value=0, max_value=3000001)
     gender = serializers.ChoiceField(choices=('m', 'f'))
     city = serializers.ListField()
     marital_status = serializers.ListField()
